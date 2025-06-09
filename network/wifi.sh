5f9a15c84732d0951da6713bfc05ab9253598a7d
fsncps <fsncps@eml.cc>
2025-06-09 21:13:13 +0200

    wifi/eth

--- network/wifi.sh
+++ network/wifi.sh
@@ -5,8 +5,14 @@ export PATH=$PATH:/sbin:/usr/sbin
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 while true; do
-   echo "$(curl -s ifcfg.me) through $(iwgetid -r) @ $(nslookup $(ip route | grep default | awk '{print $3}') | grep 'name = ' | awk -F' = ' '{print $2}' | sed 's/\.$//' | head -n 1) ($(iwconfig wlan0 | grep -oP '(?<=Signal level=)-?\d+ dBm'))" >$DIR/wifi.txt
+   if iwgetid -r >/dev/null 2>&1; then
+      SSID=$(iwgetid -r)
+      SIGNAL=$(iwconfig wlan0 | grep -oP '(?<=Signal level=)-?\d+ dBm')
+      GW_HOST=$(nslookup "$(ip route | awk '/default/ {print $3}')" | awk -F' = ' '/name =/ {gsub(/\.$/, "", $2); print $2; exit}')
+      echo "$(curl -s ifcfg.me) through $SSID @ $GW_HOST ($SIGNAL)" >"$DIR/wifi.txt"
+   else
+      GW=$(ip route | awk '/default/ {print $3}')
+      echo "$(curl -s ifcfg.me) through ethernet ($GW)" >"$DIR/wifi.txt"
+   fi
    sleep 2
 done
-# echo "$(curl -s ifcfg.me) / $(ip -4 addr show wlan0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}') on $(iwgetid -r): $(nslookup $(ip route | grep default | awk '{print $3}') | grep 'name = ' | awk -F' = ' '{print $2}' | sed 's/\.$//' | head -n 1) ($(iwconfig wlan0 | grep -oP '(?<=Signal level=)-?\d+ dBm'))" >/opt/scripts/infoscreen/network/wifi.txt
-# sleep 2
